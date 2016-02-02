7846ec01b8f0caa4632215acbca97fe612a097b8
Júlio Kamizato <jkamizato@gmail.com>
2016-02-02 11:34:01 -0200

    Refatorado, criado a classe tabuleiro

--- game.rb
+++ game.rb
@@ -1,4 +1,5 @@
 require 'gosu'
+require_relative 'tabuleiro'
 
 class Game < Gosu::Window
   attr_reader :jogador1, :jogador2, :quem_joga, :quem_venceu
@@ -24,56 +25,6 @@ class Game < Gosu::Window
     #if Gosu::button_down?Gosu
   end
 
-  def inicia_jogador
-    @jogador1 = {nome: "Jogador 1", jogadas: []}
-    @jogador2 = {nome: "Jogador 1", jogadas: []}
-    @quem_joga = 1
-    @quem_venceu = nil
-  end
-
-
-
-  def jogadas_vencedoras
-    #possiveis jogadas vencedoras
-    @vencedor = [[1, 2, 3],
-                 [4, 5, 6],
-                 [7, 8, 9],
-                 [1, 4, 7],
-                 [2, 5, 8],
-                 [3, 6, 9],
-                 [1, 5, 9],
-                 [3, 5, 7]]
-
-  end
-
-
-  def verifica_se_jogador_ganhou(jogador)
-    @vencedor.each do |row|
-      if verifica_jogada_ganhadora(row, jogador)
-        @quem_venceu = jogador
-        return true
-      end
-    end
-    false
-  end
-
-  def verifica_jogada_ganhadora(linha, jogador)
-    tem = 0
-    linha.each do |i|
-      if jogador[:jogadas].include? i
-        tem += 1
-      end
-    end
-    tem == 3
-  end
-
-  def has_vencedor
-    if verifica_se_jogador_ganhou(@jogador1) || verifica_se_jogador_ganhou(@jogador2)
-      return true
-    else
-      return false
-    end
-  end
 
   # Posicoes
   # 1 2 3
@@ -85,66 +36,17 @@ class Game < Gosu::Window
   # 2 é circle
 
 
-  def inicializa_tabuleiro
-    @tabuleiro = Hash.new
-    for posicao in 1..9
-      @tabuleiro[posicao] = 0
-    end
-  end
-
-
-  def inicia_posicao
-    @posicao[1] = [0, 0];
-  end
-
-
-
   def desenha_jogada(posicao, desenho)
     @jogada.draw(0,0,1)
   end
 
-
-  def debug
-
-  end
-
-
   def play
-    while has_vencedor == false
-      puts "Jogador #{@quem_joga}, faça sua jogada"
-        if @quem_joga == 1
-          @jogador1[:jogadas] << gets.chomp.to_i
-        else
-          @jogador2[:jogadas] << gets.chomp.to_i
-        end
-      muda_jogada_para_outro
-    end
-
-    puts "Quem venceu foi o jogador: #{@quem_venceu[:nome]}"
-
-  end
-
-
-  def muda_jogada_para_outro
-    if @quem_joga == 1
-      @quem_joga = 2
-    else
-      @quem_joga = 1
-    end
+    meu_tabuleiro = Tabuleiro.new
+    meu_tabuleiro.play
   end
 
 end
 
 window = Game.new
 #window.show
-window.inicializa_tabuleiro
-window.inicia_jogador
-window.jogadas_vencedoras
-#window.desenha_jogada(1, "cross")
-window.debug
-
-#window.jogador1 << 1
-#window.jogador1 << 4
-#window.jogador1 << 7
-
 window.play
---
+++ tabuleiro.rb
@@ -0,0 +1,93 @@
+class Tabuleiro
+
+  def initialize
+    jogadas_vencedoras
+    inicia_jogador
+  end
+
+  def jogadas_vencedoras
+    #possiveis jogadas vencedoras
+    @vencedor = [[1, 2, 3],
+                 [4, 5, 6],
+                 [7, 8, 9],
+                 [1, 4, 7],
+                 [2, 5, 8],
+                 [3, 6, 9],
+                 [1, 5, 9],
+                 [3, 5, 7]]
+
+  end
+
+  def verifica_se_jogador_ganhou(jogador)
+    @vencedor.each do |row|
+      if verifica_jogada_ganhadora(row, jogador)
+        @quem_venceu = jogador
+        return true
+      end
+    end
+    false
+  end
+
+  def verifica_jogada_ganhadora(linha, jogador)
+    tem = 0
+    linha.each do |i|
+      if jogador[:jogadas].include? i
+        tem += 1
+      end
+    end
+    tem == 3
+  end
+
+  def has_vencedor
+    if verifica_se_jogador_ganhou(@jogador1) || verifica_se_jogador_ganhou(@jogador2)
+      return true
+    else
+      return false
+    end
+  end
+
+
+  def inicializa_tabuleiro
+    @tabuleiro = Hash.new
+    for posicao in 1..9
+      @tabuleiro[posicao] = 0
+    end
+  end
+
+
+  def inicia_posicao
+    @posicao[1] = [0, 0];
+  end
+
+
+  def muda_jogada_para_outro
+    if @quem_joga == 1
+      @quem_joga = 2
+    else
+      @quem_joga = 1
+    end
+  end
+
+  def inicia_jogador
+    @jogador1 = {nome: "Jogador 1", jogadas: []}
+    @jogador2 = {nome: "Jogador 1", jogadas: []}
+    @quem_joga = 1
+    @quem_venceu = nil
+  end
+
+  def play
+    while has_vencedor == false
+      puts "Jogador #{@quem_joga}, faça sua jogada"
+      if @quem_joga == 1
+        @jogador1[:jogadas] << gets.chomp.to_i
+      else
+        @jogador2[:jogadas] << gets.chomp.to_i
+      end
+      muda_jogada_para_outro
+    end
+
+    puts "Quem venceu foi o jogador: #{@quem_venceu[:nome]}"
+
+  end
+
+end
